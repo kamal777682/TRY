930673acfbdbf32e0c7929060a2af867e337e11b
kamal777682 <60160730+kamal777682@users.noreply.github.com>
2020-01-24 20:31:06 -0500

    Init Commit

---
+++ src/main/java/com/lambton/TryThis.java
@@ -0,0 +1,8 @@
+package com.lambton;
+
+public class TryThis {
+    public static void main(String[] args) {
+        System.out.println("Kamalpreet Kaur");
+        System.out.println("Try again");
+    }
+}
